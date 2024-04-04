07f2adf708af6846da7952fbb8722a1788104048
Mohid Meer <mohidfauji@gmail.com>
2024-04-05 00:50:08 +0500

    updated

--- pages/api/stripe/checkout.js
+++ pages/api/stripe/checkout.js
@@ -10,6 +10,8 @@ export default async function handler(req, res) {
         res.status(401).json({ message: 'Unauthorized' });
     }
 
+    let customer = ''
+
     
     try {
         customer = await stripe.customers.list({ email: session.user.email, limit: 1 });
